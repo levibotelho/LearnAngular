c15aac1a7277ac832aefd1872c6eafe382bf9cf2
LeviBotelho <levi_botelho@hotmail.com>
2014-02-08 22:01:36 +0100

    Added UT project

---
+++ AngularTutorial.Tests.Unit/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AngularTutorial.Tests.Unit
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
