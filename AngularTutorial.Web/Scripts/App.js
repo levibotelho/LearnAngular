2c08e5a6090d0e106d60fe9cdac753cd1e4609f3
Levi Botelho <levi.botelho@satimo.fr>
2014-02-27 09:26:15 +0100

    Fixed initial route bug

--- AngularTutorial.Web/Scripts/App.js
+++ AngularTutorial.Web/Scripts/App.js
@@ -6,10 +6,7 @@
                   templateUrl: '/Content/Views/Lesson.html',
                   controller: 'lesson'
               }).
-              otherwise({
-                  templateUrl: '/Content/Views/Lesson.html',
-                  controller: 'lesson'
-              });
+              otherwise({redirectTo: "/lessons/cceefa09-dac9-42b1-b4b1-d74abacda62b"});
 
             $locationProvider.html5Mode(false).hashPrefix('!');
         }
