005d23566532fd4196cd8054b75e20335bf1f0f3
Levi Botelho <levi_botelho@hotmail.com>
2014-04-22 14:02:29 +0200

    Improved script include support and added  lesson example (no instructions)

---
+++ AngularTutorial.Web/Controllers/ResourceLessonController.cs
@@ -0,0 +1,51 @@
+﻿using System.Web.Mvc;
+
+namespace AngularTutorial.Web.Controllers
+{
+    public class ResourceLessonController : Controller
+    {
+        [HttpGet]
+        public JsonResult Users(int id)
+        {
+            return id == 1 ? Json(GetUser(), JsonRequestBehavior.AllowGet) : null;
+        }
+
+        [HttpPost]
+        public JsonResult Users(User user)
+        {
+            if (user.id == 1)
+            {
+                SetUser(user);
+                return Json(true);
+            }
+
+            return Json(false);
+        }
+
+        User GetUser()
+        {
+            return (User)(Session["ResourceLesson.User"] ?? (Session["ResourceLesson.User"] = new User()));
+        }
+
+        void SetUser(User user)
+        {
+            Session["ResourceLesson.User"] = user;
+        }
+	}
+
+    public class User
+    {
+        public User()
+        {
+            id = 1;
+            name = "John Doe";
+            email = "jdoe@example.com";
+        }
+
+        // ReSharper disable InconsistentNaming
+        public int id { get; set; }
+        public string name { get; set; }
+        public string email { get; set; }
+        // ReSharper restore InconsistentNaming
+    }
+}
